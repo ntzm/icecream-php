ebc2445dba305e63c880fe2e2ffea467453e0a8d
Nat Zimmermann <nat@ntzm.io>
2019-04-26 23:05:22 +0100

    small refactor

--- src/functions.php
+++ src/functions.php
@@ -16,13 +16,13 @@ function ic() {
             return null;
         }
 
-        $string = basename($caller['file']) . ':' . $caller['line'];
+        $string = basename($caller['file']) . ":{$caller['line']}";
 
         if (isset($inside['class'])) {
             $class = strpos($inside['class'], 'class@anonymous') === 0 ? 'class@anonymous' : $inside['class'];
-            $string .= ' in ' . $class . $inside['type'] . $inside['function'] . '()';
+            $string .= " in {$class}{$inside['type']}{$inside['function']}()";
         } elseif (isset($inside['function'])) {
-            $string .= ' in ' . $inside['function'] . '()';
+            $string .= " in {$inside['function']}()";
         }
 
         $output($string);
@@ -48,37 +48,68 @@ function ic() {
         throw UntraceableCall::couldNotOpen($caller['file']);
     }
 
+    $tokenCount = count($tokens);
     $functionNameIndex = null;
-    $insideOpenBrace = false;
+
+    // STEP 1: Find the function name
+    // e.g. ic('foo')
+    //      ^^
+    // The first token will always be the opening tag, or HTML before the opening tag, so we can safely skip it
+    for ($i = 1; $i < $tokenCount; ++$i) {
+        $token = $tokens[$i];
+
+        if (
+            $token[0] === T_STRING
+            && $token[2] === $caller['line']
+            && strtolower($token[1]) === 'ic'
+        ) {
+            $functionNameIndex = $i;
+            break;
+        }
+    }
+
+    if ($functionNameIndex === null) {
+        throw UntraceableCall::couldNotReadContentsOfCall($caller['file'], $caller['line']);
+    }
+
+    $openBraceIndex = null;
+
+    // STEP 2: Find the function call opening brace
+    // e.g. ic('foo')
+    //        ^
+    for ($i = $functionNameIndex + 1; $i < $tokenCount; ++$i) {
+        if ($tokens[$i] === '(') {
+            $openBraceIndex = $i;
+            break;
+        }
+    }
+
+    if ($openBraceIndex === null) {
+        throw UntraceableCall::couldNotReadContentsOfCall($caller['file'], $caller['line']);
+    }
+
     $braceDepth = 0;
     $contents = [];
 
-    foreach ($tokens as $i => $token) {
-        if ($functionNameIndex === null) {
-            if (is_array($token) && $token[0] === T_STRING && $token[2] === $caller['line'] && strtolower($token[1]) === 'ic') {
-                $functionNameIndex = $i;
-                continue;
-            }
-        } elseif (! $insideOpenBrace) {
-            if ($token === '(') {
-                $insideOpenBrace = true;
-                continue;
-            }
-        } else {
-            if ($token === '(') {
-                ++$braceDepth;
-            }
+    // STEP 2: Find all the tokens between the opening brace and the closing brace
+    // e.g. ic('foo')
+    //         ^^^^^
+    for ($i = $openBraceIndex + 1; $i < $tokenCount; ++$i) {
+        $token = $tokens[$i];
 
-            if ($token === ')') {
-                if ($braceDepth === 0) {
-                    break;
-                }
-
-                --$braceDepth;
-            }
-
-            $contents[] = is_array($token) ? $token[1] : $token;
+        if ($token === '(') {
+            ++$braceDepth;
         }
+
+        if ($token === ')') {
+            if ($braceDepth === 0) {
+                break;
+            }
+
+            --$braceDepth;
+        }
+
+        $contents[] = is_array($token) ? $token[1] : $token;
     }
 
     if ($contents === []) {
